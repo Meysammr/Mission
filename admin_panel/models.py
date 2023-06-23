d66cb35a5b4bf3e4a45f03e6b1a54511ed725013
meysam <meysam1368volleyball@gmail.com>
2023-06-23 14:10:11 +0330

    edit models and serializers

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -2,28 +2,19 @@ from django.db import models
 
 class Driver(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    driver_id = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=11)
-    is_available = models.BooleanField(default=True)
+    is_busy = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
     
-    def current_mission(self):
-        return Mission.objects.filter(assigned_driver=self).first()
-
-    def past_missions(self):
-        return Mission.objects.filter(assigned_driver=self, is_completed=True)
     
 
-
 class Mission(models.Model):
-    assigned_driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
-    mission_id = models.AutoField(primary_key=True)
-    source_address = models.CharField(max_length=200)
-    destination_address = models.CharField(max_length=200)
-    price = models.IntegerField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    pickup_address = models.CharField(max_length=100)
+    delivery_address = models.CharField(max_length=100)
+    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f'Mission {self.mission_id}'
+        return self.title
